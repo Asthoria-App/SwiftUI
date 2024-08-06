34cf75b0d8377ff7b42aa3b4c81b863c763e7af5
aysemacam <aysema584@gmail.com>
2024-08-06 10:36:10 +0300

    inital_commit

---
+++ Meditation_swiftUI/Meditation_swiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Meditation_swiftUI
+//
+//  Created by Aysema Çam on 6.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Meditation_swiftUI/Meditation_swiftUI/Meditation_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meditation_swiftUIApp.swift
+//  Meditation_swiftUI
+//
+//  Created by Aysema Çam on 6.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct Meditation_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
