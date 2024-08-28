bb95addc67dcf94d0e9514c7f776ca27a0282c05
aysemacam <aysema584@gmail.com>
2024-08-28 18:07:41 +0300

    last update with eyedrop and without libraries

--- story_photo_edit/story_photo_edit/story_photo_editApp.swift
+++ story_photo_edit/story_photo_edit/story_photo_editApp.swift
@@ -10,9 +10,6 @@ import SwiftUI
 
 @main
 struct story_photo_editApp: App {
-    // Register the custom AppDelegate
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-
     var body: some Scene {
         WindowGroup {
             StoryEditView()
@@ -20,16 +17,3 @@ struct story_photo_editApp: App {
     }
 }
 
-import UIKit
-import IQKeyboardManagerSwift
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        IQKeyboardManager.shared.enable = false
-        IQKeyboardManager.shared.enableAutoToolbar = true
-        IQKeyboardManager.shared.resignOnTouchOutside = false
-        IQKeyboardManager.shared.keyboardDistanceFromTextField = 10
-        return true
-    }
-}
