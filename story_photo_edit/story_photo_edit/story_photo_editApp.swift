d9b43bef0e80b9c11975db7f4dc3e2e4a34cf5a5
aysemacam <aysema584@gmail.com>
2024-09-17 09:48:54 +0300

    update

--- story_photo_edit/story_photo_edit/story_photo_editApp.swift
+++ story_photo_edit/story_photo_edit/story_photo_editApp.swift
@@ -28,48 +28,41 @@ class ARFaceFilterViewController: UIViewController, ARSCNViewDelegate {
         sceneView = ARSCNView(frame: self.view.frame)
         sceneView.delegate = self
         sceneView.automaticallyUpdatesLighting = true
-        
-        // Configure face tracking
         let configuration = ARFaceTrackingConfiguration()
         sceneView.session.run(configuration)
         view.addSubview(sceneView)
     }
     
-    // Add glasses when the face is detected
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
         guard let faceAnchor = anchor as? ARFaceAnchor else { return nil }
         
         let faceNode = SCNNode()
         
-        // Add 2D glasses image to the face
         let glassesNode = createGlassesNode()
         faceNode.addChildNode(glassesNode)
         
         return faceNode
     }
     
-    // Function to create a glasses node using a 2D image
     private func createGlassesNode() -> SCNNode {
         let glassesNode = SCNNode()
         
-        // Load the glasses image from assets
-        let glassesImage = UIImage(named: "aaa")!
-        let glassesPlane = SCNPlane(width: 0.15, height: 0.05) // Adjust the size to fit the face
+        let glassesImage = UIImage(named: "effect1")!
+        let glassesPlane = SCNPlane(width: 0.15, height: 0.15)
         
         let glassesMaterial = SCNMaterial()
         glassesMaterial.diffuse.contents = glassesImage
         glassesPlane.materials = [glassesMaterial]
         
         let glassesImageNode = SCNNode(geometry: glassesPlane)
-        glassesImageNode.position = SCNVector3(0, 0.02, 0.08) // Position it in front of the face
+        glassesImageNode.position = SCNVector3(0, 0.02, 0.08)
         
-        // Attach glasses node
+
         glassesNode.addChildNode(glassesImageNode)
         
         return glassesNode
     }
     
-    // Update the position and orientation of the glasses node based on the face anchor
     func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
         guard let faceAnchor = anchor as? ARFaceAnchor else { return }
         
